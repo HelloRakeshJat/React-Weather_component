bfbc0171710478c16a09af84b9845cebe9179dc0
Rakesh Jat <59819556+HelloRakeshJat@users.noreply.github.com>
2023-06-22 15:51:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import WeatherComponent from './WeatherComponent/WeatherComponent';
 
 function App() {
-  const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
-
-  const weatherComponents = [
-    { id: 1, image: "https://i0.wp.com/evs-translations.com/blog/wp-content/uploads/2019/09/Foggy_EVS-Translations_AS_L_XS_137726792.jpg?w=389&ssl=1", },
-    { id: 2, image: "https://st.depositphotos.com/1903923/1678/v/950/depositphotos_16785893-stock-illustration-sunny-day-background.jpg", },
-    { id: 3, image: "https://st4.depositphotos.com/20524830/25866/i/1600/depositphotos_258667804-stock-photo-close-shot-picture-taken-aalesund.jpg" }
-  ];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentComponentIndex((prevIndex) => (prevIndex + 1) % weatherComponents.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>Image component will re-render over when img change</h2>
-      <WeatherComponent image={weatherComponents[currentComponentIndex].image} />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <WeatherComponent />
     </div>
   );
 }
--- src/WeatherComponent/WeatherComponent.js
+++ src/WeatherComponent/WeatherComponent.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
+import axios from 'axios';
+import { Spin, Input, Alert } from 'antd';
 
 const WeatherComponent = (props) => {
   console.log("WeatherComponent props ==>", props)
-  let { image } = props
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [location, setLocation] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    if (weatherData) {
+      fetchImage(weatherData.current.condition.text);
+    }
+
+  }, [weatherData]);
+
+  const fetchImage = (query) => {
+    const unsplashAccessKey = '2Z2_-VGs_juiK4R1yd4INiG9inp17tbU6_BY0wDyy6o';
+    axios
+      .get(`https://api.unsplash.com/photos/random?query=${query}&client_id=${unsplashAccessKey}`)
+      .then((response) => {
+        const image = response.data.urls.regular;
+        setImageUrl(image);
+        console.log("fetch api imgae", image)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleSearch = () => {
+    setLoading(true);
+    setError(null);
+    setWeatherData(null);
+
+    axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=ee8ad90872a7451bac994150232206&q=${location}`)
+      .then((response) => {
+        setWeatherData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  };
 
   return (
     <div>
-      <img src={image} alt="Weather" className='image'/>
+      <Input.Search
+        placeholder="Enter location"
+        enterButton="Search"
+        size="large"
+        onSearch={handleSearch}
+        onChange={(e) => setLocation(e.target.value)}
+        style={{ width: 300, marginBottom: 16 }}
+      />
+
+      {loading && <Spin size="large" />}
+
+      {error && <Alert type="error" message={error} showIcon />}
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.location.name}</h2>
+          <p>Current temperature: {weatherData.current.temp_c}°C</p>
+          <p>Condition: {weatherData.current.condition.text}</p>
+          {imageUrl && <img src={imageUrl} alt="Weather condition" style={{ maxWidth: '50%', maxHeight: '400px' }} />}
+        </div>
+      )}
     </div>
   )
 };
