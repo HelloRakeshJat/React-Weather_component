b3f018553c8b28dc146cae6f1472fbe9306ea110
Rakesh Jat <59819556+HelloRakeshJat@users.noreply.github.com>
2023-06-23 12:36:31 +0530

    added Temperature Precipitation Wind

--- src/WeatherComponent/WeatherComponent.js
+++ src/WeatherComponent/WeatherComponent.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import '../App.css';
 import axios from 'axios';
-import { Spin, Input, Alert } from 'antd';
+import { Spin, Input, Alert, Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 const WeatherComponent = () => {
   const [loading, setLoading] = useState(false);
@@ -10,9 +12,6 @@ const WeatherComponent = () => {
   const [location, setLocation] = useState('');
   const [iconUrl, setIconUrl] = useState('');
   const [hourlyData, setHourlyData] = useState([]);
-  const dateObj = new Date(weatherData && weatherData.forecast && weatherData.forecast.forecastday[0].date);
-  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const dayOfWeek = weekdays[dateObj.getDay()];
 
   const handleSearch = () => {
     setLoading(true);
@@ -35,11 +34,25 @@ const WeatherComponent = () => {
       });
   };
 
+  const renderHourlyData = (dataKey, unit) => {
+    return (
+      <div className="hourly-list">
+        {hourlyData.map((hourData) => (
+          <div className="hourly-item" key={hourData.time}>
+            <p>{hourData.time.split(' ')[1]}</p>
+            <img src={hourData.condition.icon} alt="Hourly Weather Icon" />
+            <p>{hourData[dataKey]} {unit}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div className='weather-data'>
+    <div className="weather-data">
       <Input.Search
         placeholder="Enter location"
-        className='search-data'
+        className="search-data"
         enterButton="Search"
         size="large"
         onSearch={handleSearch}
@@ -56,16 +69,20 @@ const WeatherComponent = () => {
           <p>Current temperature: {weatherData.current.temp_c}°C</p>
           <p>Condition: {weatherData.current.condition.text}</p>
           {iconUrl && <img src={iconUrl} alt="Weather icon" />}
-          <h3>Hourly Weather Data for {weatherData.forecast.forecastday[0].date} {dayOfWeek}</h3>
-          <ul className="hourly-list">
-            {hourlyData.map((hourData) => (
-              <li className="hourly-item">
-                <p>{hourData.time.split(' ')[1]}</p>
-                <img src={hourData.condition.icon} alt="Hourly Weather Icon" />
-                <p>{hourData.temp_c}°C</p>
-              </li>
-            ))}
-          </ul>
+          <h3>
+            Hourly Weather Data for {weatherData.forecast.forecastday[0].date} {new Date().toLocaleDateString('en-US', { weekday: 'long' })}
+          </h3>
+          <Tabs defaultActiveKey="temperature">
+            <TabPane tab="Temperature" key="temperature">
+              {renderHourlyData('temp_c', '°C')}
+            </TabPane>
+            <TabPane tab="Precipitation" key="precipitation">
+              {renderHourlyData('humidity', '%')}
+            </TabPane>
+            <TabPane tab="Wind" key="wind">
+              {renderHourlyData('wind_kph', 'km/h')}
+            </TabPane>
+          </Tabs>
         </div>
       )}
     </div>
